e60f2f2ddede21ae64acd034abb9f7db7e37f72f
pratik.gawande <you@example.com>
2026-06-13 22:50:43 +0530

    Added user

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,8 @@
 // Using promise
 const asyncHandler = (fn)=>{
-    (req,res,next) =>{
-        Promise.resolve(fn(req,res,next)).catch(
-            (err)=> next(err)
+    return (req,res,next) =>{
+                Promise.resolve(fn(req,res,next)).catch(
+                (err)=> next(err)
         )
 
     }
