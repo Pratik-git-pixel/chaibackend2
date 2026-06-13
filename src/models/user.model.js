0e6ce9522670a473881f466d58b939cc1e3ff480
pratik.gawande <you@example.com>
2026-06-14 00:00:10 +0530

    Bug fix and added user controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,7 +54,7 @@ const userSchema = new mongoose.Schema({
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password,10)
+    this.password = await bcrypt.hash(this.password,10)
     next()
     
 })
@@ -78,7 +78,7 @@ userSchema.methods.generateAccessToken = async function(){
     }
 )}
 
-userSchema.methods.generateRefreshToken = function(){
+userSchema.methods.generateRefreshToken = async function(){
      return await jwt.sign({
         _id:this._id
     },
